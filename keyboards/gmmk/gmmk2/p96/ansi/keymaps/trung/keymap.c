e8b69bb53d1b718d70ba5d599f652cae5ccda568
trngm <63921509+trngm@users.noreply.github.com>
2024-12-22 22:43:12 -0500

    2024 12 22

--- keyboards/gmmk/gmmk2/p96/ansi/keymaps/trung/keymap.c
+++ keyboards/gmmk/gmmk2/p96/ansi/keymaps/trung/keymap.c
@@ -39,22 +39,22 @@ enum  {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {  
 	[0] = LAYOUT(   
-        KC_ESC,                     KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,     KC_F7,   KC_F8,    KC_F9,      KC_F10,     KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_MPLY,    KC_MUTE,    KC_VOLD,    KC_VOLU, 
-        TD(TD_DEL),                 KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,      KC_7,    KC_8,     KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ TD(TD_CMD), TD(TD_2),   KC_ASTR,    TD(TD_4), 
-        KC_TAB,                     TD(TD_Q),               TD(TD_W),           TD(TD_E),   KC_R,           /**/ KC_T,     KC_Y,      KC_U,    KC_I,     KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,      KC_P8,      KC_P9,      TD(TD_5), 
-        KC_CAPS,                    KC_A,                   KC_S,               KC_D,       KC_F,           /**/ KC_G,     KC_H,      KC_J,    KC_K,     KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,                             /**/ KC_P4,      KC_P5,      KC_P6,      
-        KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ KC_B,     KC_N,      KC_M,    KC_COMM,  KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,                  /**/ KC_P1,      KC_P2,      KC_P3,      KC_PENT,    
-        KC_LCTL,                    LT(2,KC_LGUI),          KC_LALT,            LT(1,KC_SPC),               /**/                               KC_RALT,  MO(3),      KC_RCTL,                KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,                  TD(TD_7)
-    ),              
-                
-	[1] = LAYOUT( //Spacebar                
-
-        QK_BOOT,                    KC_F13,                 KC_F14,             KC_F15,     KC_F16,         /**/ KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,     KC_F22,     KC_F23,     KC_F24,     KC_PSCR,                /**/ KC_DEL,    KC_INS,    KC_PGUP,   KC_PGDN,
-        KC_0,                       KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,       KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS,
-        KC_TAB,                     KC_4,                   KC_5,               KC_6,       KC_BSPC,        /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,       KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,     KC_PPLS,
-        KC_CAPS,                    KC_7,                   KC_8,               KC_9,       KC_SPC,         /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,       KC_SCLN,    KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,     KC_P6, 
-        KC_LSFT,                    TD(TD_4),               TD(TD_2),           TD(TD_3),   KC_DOT,         /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,     KC_SLSH,    KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,     KC_P3,     KC_PENT,
-        KC_LCTL,                    KC_LGUI,                LT(0,KC_LALT),      KC_SPC,                     /**/                                         KC_RALT,    MO(3),      KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,                KC_PDOT 
+        KC_ESC,                     KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,     KC_F7,   KC_F8,    KC_F9,       KC_F10,     KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_MPLY,    KC_MUTE,    KC_VOLD,    KC_VOLU, 
+        TD(TD_DEL),                 KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,      KC_7,    KC_8,     KC_9,        KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ TD(TD_CMD), TD(TD_2),   KC_ASTR,    TD(TD_4), 
+        KC_TAB,                     TD(TD_Q),               TD(TD_W),           TD(TD_E),   KC_R,           /**/ KC_T,     KC_Y,      KC_U,    KC_I,     KC_O,        KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,      KC_P8,      KC_P9,      TD(TD_5), 
+        KC_CAPS,                    KC_A,                   KC_S,               KC_D,       KC_F,           /**/ KC_G,     KC_H,      KC_J,    KC_K,     KC_L,        KC_SCLN,    KC_QUOT,    KC_ENT,                             /**/ KC_P4,      KC_P5,      KC_P6,      
+        KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ KC_B,     KC_N,      KC_M,    KC_COMM,  KC_DOT,      KC_SLSH,    KC_RSFT,                KC_UP,                  /**/ KC_P1,      KC_P2,      KC_P3,      KC_PENT,    
+        KC_LCTL,                    LT(2,KC_LGUI),          KC_LALT,            LT(1,KC_SPC),               /**/                               KC_RALT,  MO(3),       KC_RCTL,                KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,                  TD(TD_7)
+    ),               
+                 
+	[1] = LAYOUT( //Spacebar                 
+ 
+        QK_BOOT,                    KC_F13,                 KC_F14,             KC_F15,     KC_F16,         /**/ KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,      KC_F22,     KC_F23,     KC_F24,     KC_PSCR,                /**/ KC_DEL,    KC_INS,    KC_PGUP,   KC_PGDN,
+        KC_0,                       KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS,
+        KC_TAB,                     KC_4,                   KC_5,               KC_6,       KC_BSPC,        /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,     KC_PPLS,
+        KC_CAPS,                    KC_7,                   KC_8,               KC_9,       KC_SPC,         /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,    KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,     KC_P6, 
+        KC_LSFT,                    TD(TD_4),               TD(TD_2),           TD(TD_3),   KC_DOT,         /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,    KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,     KC_P3,     KC_PENT,
+        KC_LCTL,                    KC_LGUI,                LT(0,KC_LALT),      KC_SPC,                     /**/                                         KC_RALT,     MO(3),      KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,                KC_PDOT 
     ),                      
                     
     [2] = LAYOUT( //Win Key                                     
@@ -76,8 +76,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),              
                 
     [4] = LAYOUT(                           
-        DF(0),                      DF(1),                  DF(2),              DF(3),      DF(4),          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_DEL,    DT_PRNT,   DT_UP,    DT_DOWN,
-        KC_GRV,                     KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      RGB_VAD,    RGB_VAI,    KC_BSPC,                /**/ KC_HOME,   KC_PSLS,   KC_PAST,  KC_PMNS,
+        KC_ESC,                     KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_DEL,    DT_PRNT,   DT_UP,    DT_DOWN,
+        TD(TD_DEL),                 KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      RGB_VAD,    RGB_VAI,    KC_BSPC,                /**/ KC_HOME,   KC_PSLS,   KC_PAST,  KC_PMNS,
         KC_TAB,                     KC_Q,                   KC_W,               KC_E,       KC_R,           /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P ,     KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,    KC_PPLS,
         KC_CAPS,                    KC_A,                   KC_S,               KC_D,       KC_F,           /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,   KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,     KC_P6,
         KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,   KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,     KC_P3,    KC_PENT,
@@ -131,7 +131,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 
 // Tap Dance Codes
-
     tap_dance_action_t *action;
 
     switch (keycode) {  // list all tap dance keycodes with tap-hold configurations
@@ -204,33 +203,69 @@ tap_dance_action_t tap_dance_actions[] = {
 
 
 // Combo Codes
-const uint16_t PROGMEM combo1[] = {KC_CAPS, KC_1, COMBO_END};
+    const uint16_t PROGMEM combo1[] = {KC_CAPS, KC_1, COMBO_END};
 
-enum combo_events {
-  CMBE1,
-  CMBE2,
-};
+    enum combo_events {
+    CMBE1,
+    CMBE2,
+    };
 
-const uint16_t PROGMEM CMB1[] = {KC_CAPS, KC_1, COMBO_END};
-const uint16_t PROGMEM CMB2[] = {KC_CAPS, KC_2, COMBO_END};
+    const uint16_t PROGMEM CMB1[] = {KC_CAPS, KC_1, COMBO_END};
+    const uint16_t PROGMEM CMB2[] = {KC_CAPS, KC_2, COMBO_END};
 
-combo_t key_combos[] = {
-  [CMBE1] = COMBO_ACTION(CMB1),
-  [CMBE2] = COMBO_ACTION(CMB2),
-};
-/* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
+    combo_t key_combos[] = {
+    [CMBE1] = COMBO_ACTION(CMB1),
+    [CMBE2] = COMBO_ACTION(CMB2),
+    };
+    /* COMBO_ACTION(x) is same as COMBO(x, KC_NO) */
 
-void process_combo_event(uint16_t combo_index, bool pressed) {
-  switch(combo_index) {
-    case CMBE1:
-      if (pressed) {
-        SEND_STRING("1/4");
-      }
-      break;
-    case CMBE2:
-      if (pressed) {
-        SEND_STRING("½");
-      }
-      break;
-  }
+    void process_combo_event(uint16_t combo_index, bool pressed) {
+    switch(combo_index) {
+        case CMBE1:
+        if (pressed) {
+            SEND_STRING("1/4");
+        }
+        break;
+        case CMBE2:
+        if (pressed) {
+            SEND_STRING("½");
+        }
+        break;
+    }
+    }
+
+
+// Lighting Layers 
+
+
+// Light LEDs 6 to 9 and 12 to 15 red when caps lock is active. Hard to ignore!
+const rgblight_segment_t PROGMEM my_capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {6, 4, HSV_RED},       // Light 4 LEDs, starting with LED 6
+    {12, 4, HSV_RED}       // Light 4 LEDs, starting with LED 12
+);
+// Light LEDs 9 & 10 in cyan when keyboard layer 1 is active
+const rgblight_segment_t PROGMEM my_layer1_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {9, 2, HSV_CYAN}
+);
+// Light LEDs 11 & 12 in purple when keyboard layer 2 is active
+const rgblight_segment_t PROGMEM my_layer2_layer[] = RGBLIGHT_LAYER_SEGMENTS(
+    {11, 2, HSV_PURPLE}
+);
+// Light LEDs 13 & 14 in green when keyboard layer 3 is active
+const rgblight_segment_t PROGMEM 4[] = RGBLIGHT_LAYER_SEGMENTS(
+    {13, 2, HSV_GREEN}
+);
+// etc..
+
+// Now define the array of layers. Later layers take precedence
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    my_capslock_layer,
+    my_layer1_layer,    // Overrides caps lock layer
+    my_layer2_layer,    // Overrides other layers
+    4     // Overrides other layers
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
 }
