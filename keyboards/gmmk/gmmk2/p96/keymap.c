ee03a2a98b944c8f3a80f32d4a6e8118fce0d5a2
trngm <63921509+trngm@users.noreply.github.com>
2025-01-20 11:11:33 -0500

    Add files via upload

--- keyboards/gmmk/gmmk2/p96/keymap.c
+++ keyboards/gmmk/gmmk2/p96/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 
 
+
  /*
  * |-------------------------------------------------------------------------------------------------------------------------------------------------------------------------|
  * | ESC    | F1     | F2     | F3     | F4     | F5     | F6     | F7     | F8     | F9     | F10    | F11    | F12    | PRTSC  |        | PRTSC | INS   | PG UP  | PG DN   |
@@ -49,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 	[1] = LAYOUT( //Spacebar
 
-        QK_BOOT,                    KC_F13,                 KC_F14,             KC_F15,     KC_F16,         /**/ KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,      KC_F22,     KC_F23,     KC_F24,     KC_PSCR,                /**/ KC_DEL,    KC_INS,    KC_PGUP,   KC_PGDN,
-        KC_0,                       KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ KC_NUM,    KC_PSLS,   KC_PAST,   KC_PMNS,
+        QK_BOOT,                    KC_F13,                 KC_F14,             KC_F15,     KC_F16,         /**/ KC_F17,   KC_F18,   KC_F19,   KC_F20,   KC_F21,      KC_F22,     KC_F23,     KC_F24,     KC_PSCR,                /**/ KC_DEL,    KC_INS,    KC_PGDN,   KC_PGUP,
+        KC_0,                       KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,       KC_MINS,    KC_EQL,     KC_BSPC,                /**/ KC_HOME,   KC_END,    KC_PGDN,   KC_PGUP,
         KC_TAB,                     KC_4,                   KC_5,               KC_6,       KC_BSPC,        /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,       KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,     KC_PPLS,
         KC_CAPS,                    KC_7,                   KC_8,               KC_9,       KC_SPC,         /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,    KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,     KC_P6,
         KC_LSFT,                    TD(TD_4),               TD(TD_2),           TD(TD_3),   KC_DOT,         /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,    KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,     KC_P3,     KC_PENT,
@@ -60,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [2] = LAYOUT( //Win Key
         QK_GESC,                    KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_DEL,    KC_INS,   KC_PGUP,  KC_PGDN,
         KC_GRV,                     KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      KC_MINS,    KC_EQL,     KC_BSPC,                /**/ KC_NUM,    KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,                     KC_QUOT,                KC_UP,              KC_MINS,    KC_EQL,         /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,    KC_P9,    KC_PPLS,
+        KC_TAB,                     KC_LBRC,                KC_UP,              KC_RBRC,    KC_EQL,         /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,      KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,    KC_P9,    KC_PPLS,
         KC_CAPS,                    KC_LEFT,                KC_DOWN,            KC_RGHT,    KC_F,           /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,   KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,    KC_P6,
         KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,   KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,    KC_P3,    KC_PENT,
         KC_LCTL,                    KC_LGUI,                KC_LALT,            KC_SPC,                     /**/                                         KC_RALT,     MO(3),     KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,               KC_PDOT
@@ -68,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     [3] = LAYOUT( //FN Key
         TG(0),                      TG(1),                  TG(2),              TG(3),      TG(4),          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_DEL,    DT_PRNT,   DT_UP,    DT_DOWN,
-        KC_GRV,                     M_1,                    M_2,                KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      RGB_VAD,    RGB_VAI,    KC_BSPC,                /**/ KC_HOME,   KC_PSLS,   KC_PAST,  KC_PMNS,
+        KC_GRV,                     M_1,                    M_2,                KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      RGB_VAD,    RGB_VAI,    KC_BSPC,                /**/ KC_HOME,   KC_END,    KC_PGDN,  KC_PGUP,
         KC_TAB,                     KC_Q,                   KC_W,               KC_E,       KC_R,           /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P,      RGB_HUI,    RGB_HUD,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,    KC_PPLS,
         KC_CAPS,                    KC_A,                   KC_S,               KC_D,       KC_F,           /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,   RGB_SAI,    RGB_SAD,                            /**/ KC_P4,     KC_P5,     KC_P6,
         KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ RGB_TOG,  KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,   KC_RSFT,                RGB_SPI,                /**/ RGB_M_P,   RGB_M_B,   RGB_M_R,  KC_PENT,
@@ -76,14 +77,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [4] = LAYOUT(
-        KC_ESC,                     KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_DEL,    DT_PRNT,   DT_UP,    DT_DOWN,
+        KC_ESC,                     KC_F1,                  KC_F2,              KC_F3,      KC_F4,          /**/ KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,       KC_F10,    KC_F11,     KC_F12,     KC_PSCR,                /**/ KC_MPLY,   KC_MUTE,   KC_VOLD,  KC_VOLU,
         KC_GRV,                     KC_1,                   KC_2,               KC_3,       KC_4,           /**/ KC_5,     KC_6,     KC_7,     KC_8,     KC_9,        KC_0,      RGB_VAD,    RGB_VAI,    KC_BSPC,                /**/ KC_HOME,   KC_PSLS,   KC_PAST,  KC_PMNS,
         KC_TAB,                     KC_Q,                   KC_W,               KC_E,       KC_R,           /**/ KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,        KC_P ,     KC_LBRC,    KC_RBRC,    KC_BSLS,                /**/ KC_P7,     KC_P8,     KC_P9,    KC_PPLS,
         KC_CAPS,                    KC_A,                   KC_S,               KC_D,       KC_F,           /**/ KC_G,     KC_H,     KC_J,     KC_K,     KC_L,        KC_SCLN,   KC_QUOT,    KC_ENT,                             /**/ KC_P4,     KC_P5,     KC_P6,
         KC_LSFT,                    KC_Z,                   KC_X,               KC_C,       KC_V,           /**/ KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,      KC_SLSH,   KC_RSFT,                KC_UP,                  /**/ KC_P1,     KC_P2,     KC_P3,    KC_PENT,
         KC_LCTL,                    KC_LGUI,                KC_LALT,            KC_SPC,                     /**/                                         KC_RALT,     TO(0),     KC_RCTL,    KC_LEFT,    KC_DOWN,  KC_RGHT,      /**/ KC_P0,                KC_PDOT
     )
-
 
 
 /*
