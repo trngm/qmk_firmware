87bd50824bdc1fd025343876c1fe0d8f64c5440f
trngm <63921509+trngm@users.noreply.github.com>
2025-09-03 23:17:46 -0400

    Add files via upload

--- keyboards/gmmk/pro/rev2/ansi/keymaps/trung/keymap.c
+++ keyboards/gmmk/pro/rev2/ansi/keymaps/trung/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_SPACE] = LAYOUT(
         QK_GESC, KC_F11, KC_F12,   KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19, KC_F20, KC_F21, KC_F22, KC_F23,               _______,
         KC_0,    KC_1,    KC_2,    KC_3,    _______, _______, _______,    _______, _______, _______, _______, _______, _______, _______,          KC_PGUP,
-        KC_TAB,  KC_4,    KC_5,    KC_6,    KC_BSPC, KC_DQT,  M_3, MS_UP, _______, _______, _______, _______, _______, QK_BOOT,              KC_PGDN,
+        KC_TAB,  KC_4,    KC_5,    KC_6,    KC_BSPC, KC_DQT,  M_4, MS_UP, _______, _______, _______, _______, _______, QK_BOOT,              KC_PGDN,
         _______, KC_7,    KC_8,    KC_9,    KC_SPC,  KC_QUOT, MS_LEFT, MS_DOWN, MS_RGHT,    _______, _______, _______,          _______,          _______,
         _______, KC_MINS, KC_SLSH, KC_EQL,  KC_DOT,  _______, _______, _______, _______,    _______, _______,          _______, RM_NEXT, _______,
         CTL_T(KC_ENT), _______, KC_LALT,                            _______,                         _______, TO(0), _______, RM_SPDD, RM_PREV, RM_SPDU
@@ -108,12 +108,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 
     [_FN] = LAYOUT(
-        TG(0),   TG(1),   TG(2),   TG(3),   TG(4),   TG(5), _______, _______, _______, _______, _______, _______, _______, DM_RSTP,            QK_RBT,
-        _______, M_1,     M_4,     _______, _______, _______, _______, _______, _______, _______, _______, UG_SATU, UG_HUEU, _______,          DM_PLY1,
-        _______, RGB_M_SW, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,         DM_PLY2,
+        TG(0),   TG(1),   TG(2),   TG(3),   TG(4),   TG(5), _______, _______, _______, _______, _______, _______, _______, _______,            QK_RBT,
+        _______, M_1,     M_4,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          DM_PLY1,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, QK_BOOT,          DM_PLY2,
         _______, AS_ON,   AS_OFF,  _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          DM_REC1,
-        _______,          _______, _______, RGB_TOG, RM_TOGG, _______, _______, QK_MAKE, _______, _______, _______,          _______, RM_NEXT, DM_REC2,
-        _______, _______, _______,                            _______,                            _______, TO(0), _______, RM_SPDD, RM_PREV, RM_SPDU
+        _______,          _______, _______, RM_ON, RM_TOGG, _______, _______, QK_MAKE, _______, _______, _______,          _______, RM_VALD, DM_REC2,
+        _______, _______, _______,                            _______,                            _______, TO(0), _______, RM_SATD, RM_PREV, RM_SPDU
     ),
 
 
@@ -126,6 +126,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______,                            _______,                            _______, TO(0), _______, RM_SPDD, RM_PREV, RM_SPDU
     ),
 
+        [_DEFAULT] = LAYOUT(
+        KC_ESC,         KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
+        KC_DEL,         KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          TD(lay1),
+        KC_TAB,         KC_U,       KC_I,       KC_O,       KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,          TD(lay2),
+        KC_CAPS,        KC_J,       KC_K,       KC_L,       KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           TD(lay3),
+        KC_LSFT,                    KC_Z,       KC_X,       KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT, KC_UP,   KC_END,
+        CTL_T(KC_ENT),  KC_LGUI,    KC_LALT,                            KC_SPC,                                   KC_RALT, TO(0),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+
     [_DEFAULT] = LAYOUT(
         KC_ESC,         KC_F1,      KC_F2,      KC_F3,      KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,          KC_MUTE,
         KC_DEL,         KC_1,       KC_2,       KC_3,       KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,          TD(lay1),
@@ -136,6 +145,22 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
 };
+
+bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
+    if (host_keyboard_led_state().caps_lock) {
+        RGB_MATRIX_INDICATOR_SET_COLOR(3, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(224, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(86, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(82, 255, 255, 255);
+        RGB_MATRIX_INDICATOR_SET_COLOR(72, 255, 255, 255);
+
+    } else {
+        RGB_MATRIX_INDICATOR_SET_COLOR(3, 0, 0, 0);
+    }
+    return false;
+}
+
+
 // clang-format on
 
 //      #if defined(ENCODER_MAP_ENABLE)
